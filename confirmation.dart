0ab12024d86921e4e442033e62878f03d6b1dbab
Tech-neophyte <122295513+Tech-neophyte@users.noreply.github.com>
2023-09-24 04:03:19 +0530

    Create confirmation.dart

---
+++ confirmation.dart
@@ -0,0 +1,190 @@
+import '/backend/backend.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'package:barcode_widget/barcode_widget.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+import 'booking_details_profile_model.dart';
+export 'booking_details_profile_model.dart';
+
+class BookingDetailsProfileWidget extends StatefulWidget {
+  const BookingDetailsProfileWidget({
+    Key? key,
+    this.appointmentDetails,
+  }) : super(key: key);
+
+  final DocumentReference? appointmentDetails;
+
+  @override
+  _BookingDetailsProfileWidgetState createState() =>
+      _BookingDetailsProfileWidgetState();
+}
+
+class _BookingDetailsProfileWidgetState
+    extends State<BookingDetailsProfileWidget> {
+  late BookingDetailsProfileModel _model;
+
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => BookingDetailsProfileModel());
+  }
+
+  @override
+  void dispose() {
+    _model.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<AppointmentsRecord>(
+      stream: AppointmentsRecord.getDocument(widget.appointmentDetails!),
+      builder: (context, snapshot) {
+        // Customize what your widget looks like when it's loading.
+        if (!snapshot.hasData) {
+          return Scaffold(
+            backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
+            body: Center(
+              child: SizedBox(
+                width: 40,
+                height: 40,
+                child: SpinKitWanderingCubes(
+                  color: FlutterFlowTheme.of(context).tertiary,
+                  size: 40,
+                ),
+              ),
+            ),
+          );
+        }
+        final bookingDetailsProfileAppointmentsRecord = snapshot.data!;
+        return Scaffold(
+          key: scaffoldKey,
+          backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
+          appBar: AppBar(
+            backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
+            automaticallyImplyLeading: false,
+            leading: InkWell(
+              splashColor: Colors.transparent,
+              focusColor: Colors.transparent,
+              hoverColor: Colors.transparent,
+              highlightColor: Colors.transparent,
+              onTap: () async {
+                context.pop();
+              },
+              child: Icon(
+                Icons.chevron_left_rounded,
+                color: FlutterFlowTheme.of(context).grayLight,
+                size: 32,
+              ),
+            ),
+            title: Text(
+              'Confirmation',
+              style: FlutterFlowTheme.of(context).headlineSmall,
+            ),
+            actions: [],
+            centerTitle: false,
+            elevation: 0,
+          ),
+          body: Stack(
+            children: [
+              SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(20, 12, 20, 0),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Expanded(
+                            child: Align(
+                              alignment: AlignmentDirectional(1.00, 0.00),
+                              child: Row(
+                                mainAxisSize: MainAxisSize.max,
+                                children: [
+                                  Expanded(
+                                    child: Text(
+                                      'Your booking is confirmed!\n\nUse the given barcode for \nphysical verification',
+                                      textAlign: TextAlign.center,
+                                      style: FlutterFlowTheme.of(context)
+                                          .bodyMedium
+                                          .override(
+                                            fontFamily: 'Outfit',
+                                            fontSize: 22,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(0, 32, 0, 36),
+                      child: FFButtonWidget(
+                        onPressed: () async {
+                          await bookingDetailsProfileAppointmentsRecord
+                              .reference
+                              .delete();
+                          context.pop();
+                        },
+                        text: 'Cancel Booking',
+                        options: FFButtonOptions(
+                          width: 230,
+                          height: 50,
+                          padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+                          iconPadding:
+                              EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+                          color:
+                              FlutterFlowTheme.of(context).secondaryBackground,
+                          textStyle:
+                              FlutterFlowTheme.of(context).titleSmall.override(
+                                    fontFamily: 'Outfit',
+                                    color: Color(0xFFE50A0A),
+                                    fontWeight: FontWeight.normal,
+                                  ),
+                          elevation: 2,
+                          borderSide: BorderSide(
+                            color: Colors.transparent,
+                            width: 1,
+                          ),
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                    ),
+                    BarcodeWidget(
+                      data:
+                          bookingDetailsProfileAppointmentsRecord.reference.id,
+                      barcode: Barcode.code128(),
+                      width: 300,
+                      height: 90,
+                      color: FlutterFlowTheme.of(context).primaryText,
+                      backgroundColor: Colors.transparent,
+                      errorBuilder: (_context, _error) => SizedBox(
+                        width: 300,
+                        height: 90,
+                      ),
+                      drawText: true,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
