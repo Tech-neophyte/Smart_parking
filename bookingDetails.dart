48321150c593f0f31efddf5e4254f2aee4cfe35b
Tech-neophyte <122295513+Tech-neophyte@users.noreply.github.com>
2023-09-24 04:00:12 +0530

    Create bookingDetails.dart

---
+++ bookingDetails.dart
@@ -0,0 +1,220 @@
+import '/backend/backend.dart';
+import '/components/edit_booking/edit_booking_widget.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+import 'booking_details_model.dart';
+export 'booking_details_model.dart';
+
+class BookingDetailsWidget extends StatefulWidget {
+  const BookingDetailsWidget({
+    Key? key,
+    this.appointmentDetails,
+  }) : super(key: key);
+
+  final DocumentReference? appointmentDetails;
+
+  @override
+  _BookingDetailsWidgetState createState() => _BookingDetailsWidgetState();
+}
+
+class _BookingDetailsWidgetState extends State<BookingDetailsWidget> {
+  late BookingDetailsModel _model;
+
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => BookingDetailsModel());
+  }
+
+  @override
+  void dispose() {
+    _model.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<AppointmentsRecord>(
+      stream: AppointmentsRecord.getDocument(widget.appointmentDetails!),
+      builder: (context, snapshot) {
+        // Customize what your widget looks like when it's loading.
+        if (!snapshot.hasData) {
+          return Scaffold(
+            backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
+            body: Center(
+              child: SizedBox(
+                width: 40,
+                height: 40,
+                child: SpinKitWanderingCubes(
+                  color: FlutterFlowTheme.of(context).tertiary,
+                  size: 40,
+                ),
+              ),
+            ),
+          );
+        }
+        final bookingDetailsAppointmentsRecord = snapshot.data!;
+        return Scaffold(
+          key: scaffoldKey,
+          backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
+          appBar: AppBar(
+            backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
+            automaticallyImplyLeading: false,
+            leading: InkWell(
+              splashColor: Colors.transparent,
+              focusColor: Colors.transparent,
+              hoverColor: Colors.transparent,
+              highlightColor: Colors.transparent,
+              onTap: () async {
+                context.pop();
+              },
+              child: Icon(
+                Icons.chevron_left_rounded,
+                color: FlutterFlowTheme.of(context).grayLight,
+                size: 32,
+              ),
+            ),
+            title: Text(
+              'Details',
+              style: FlutterFlowTheme.of(context).headlineSmall,
+            ),
+            actions: [],
+            centerTitle: false,
+            elevation: 0,
+          ),
+          body: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(0, 28, 0, 36),
+                      child: FFButtonWidget(
+                        onPressed: () async {
+                          await showModalBottomSheet(
+                            isScrollControlled: true,
+                            backgroundColor: Color(0x00000000),
+                            barrierColor: Color(0x00000000),
+                            context: context,
+                            builder: (context) {
+                              return Padding(
+                                padding: MediaQuery.viewInsetsOf(context),
+                                child: Container(
+                                  height: 720,
+                                  child: EditBookingWidget(
+                                    userAppointment:
+                                        bookingDetailsAppointmentsRecord,
+                                  ),
+                                ),
+                              );
+                            },
+                          ).then((value) => safeSetState(() {}));
+                        },
+                        text: 'Confirm and proceed',
+                        options: FFButtonOptions(
+                          width: 300,
+                          height: 50,
+                          padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+                          iconPadding:
+                              EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+                          color: Color(0xFF14A0B3),
+                          textStyle: FlutterFlowTheme.of(context).titleSmall,
+                          elevation: 2,
+                          borderSide: BorderSide(
+                            color: Colors.transparent,
+                            width: 1,
+                          ),
+                          borderRadius: BorderRadius.circular(40),
+                        ),
+                      ),
+                    ),
+                    Text(
+                      'Payment methods\n',
+                      style: FlutterFlowTheme.of(context).bodyMedium.override(
+                            fontFamily: 'Outfit',
+                            fontSize: 29,
+                            fontWeight: FontWeight.w800,
+                          ),
+                    ),
+                  ],
+                ),
+              ),
+              Align(
+                alignment: AlignmentDirectional(0.00, 0.00),
+                child: FFButtonWidget(
+                  onPressed: () async {
+                    context.pushNamed('BookingDetailsProfile');
+                  },
+                  text: 'In-site',
+                  options: FFButtonOptions(
+                    height: 40,
+                    padding: EdgeInsetsDirectional.fromSTEB(24, 0, 24, 0),
+                    iconPadding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+                    color: FlutterFlowTheme.of(context).primary,
+                    textStyle: FlutterFlowTheme.of(context).titleSmall.override(
+                          fontFamily: 'Outfit',
+                          color: Colors.white,
+                        ),
+                    elevation: 3,
+                    borderSide: BorderSide(
+                      color: Colors.transparent,
+                      width: 1,
+                    ),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+              ),
+              Text(
+                'Pay automatcally\n via parquick wallet\n',
+                textAlign: TextAlign.center,
+                style: FlutterFlowTheme.of(context).titleMedium,
+              ),
+              Align(
+                alignment: AlignmentDirectional(0.00, 2.00),
+                child: FFButtonWidget(
+                  onPressed: () {
+                    print('Button pressed ...');
+                  },
+                  text: 'Payment gateway',
+                  options: FFButtonOptions(
+                    height: 40,
+                    padding: EdgeInsetsDirectional.fromSTEB(24, 0, 24, 0),
+                    iconPadding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+                    color: FlutterFlowTheme.of(context).primary,
+                    textStyle: FlutterFlowTheme.of(context).titleSmall.override(
+                          fontFamily: 'Outfit',
+                          color: Colors.white,
+                        ),
+                    elevation: 3,
+                    borderSide: BorderSide(
+                      color: Colors.transparent,
+                      width: 1,
+                    ),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+              ),
+              Text(
+                'Via UPI/Gpay/Card',
+                textAlign: TextAlign.center,
+                style: FlutterFlowTheme.of(context).titleMedium,
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
