2cf1d3d0e7a9c25106e2e258f3ea1c7f74926905
Tech-neophyte <122295513+Tech-neophyte@users.noreply.github.com>
2023-09-24 21:24:08 +0530

    Create EmptyList.dart

---
+++ components/EmptyList.dart
@@ -0,0 +1,113 @@
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+
+import 'empty_list_model.dart';
+export 'empty_list_model.dart';
+
+class EmptyListWidget extends StatefulWidget {
+  const EmptyListWidget({Key? key}) : super(key: key);
+
+  @override
+  _EmptyListWidgetState createState() => _EmptyListWidgetState();
+}
+
+class _EmptyListWidgetState extends State<EmptyListWidget> {
+  late EmptyListModel _model;
+
+  @override
+  void setState(VoidCallback callback) {
+    super.setState(callback);
+    _model.onUpdate();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _model = createModel(context, () => EmptyListModel());
+  }
+
+  @override
+  void dispose() {
+    _model.maybeDispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(
+          Icons.date_range_rounded,
+          color: FlutterFlowTheme.of(context).secondaryText,
+          size: 90,
+        ),
+        Padding(
+          padding: EdgeInsetsDirectional.fromSTEB(0, 24, 0, 0),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'No Bookings!',
+                textAlign: TextAlign.center,
+                style: FlutterFlowTheme.of(context).headlineSmall,
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: EdgeInsetsDirectional.fromSTEB(12, 4, 12, 0),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Text(
+                  'You are all caught! No bookings scheduled, need an spot? Schedule one now.',
+                  textAlign: TextAlign.center,
+                  style: FlutterFlowTheme.of(context).bodySmall,
+                ),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: EdgeInsetsDirectional.fromSTEB(0, 12, 0, 0),
+          child: FFButtonWidget(
+            onPressed: () async {
+              context.pushNamed('FindParking');
+            },
+            text: 'Schedule',
+            options: FFButtonOptions(
+              width: 170,
+              height: 50,
+              padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+              iconPadding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 0),
+              color: FlutterFlowTheme.of(context).primary,
+              textStyle: FlutterFlowTheme.of(context).titleSmall.override(
+                    fontFamily: 'Lexend Deca',
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.normal,
+                  ),
+              elevation: 2,
+              borderSide: BorderSide(
+                color: Colors.transparent,
+                width: 1,
+              ),
+              borderRadius: BorderRadius.circular(40),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
