b27868ac1c649ac0475387f88439004d944ad838
Aananditaa <122295513+Tech-neophyte@users.noreply.github.com>
2024-02-05 03:08:15 +0530

    Create confirmation.dart

--- confirmation.dart
+++ confirmation.dart
@@ -11,7 +11,7 @@ import 'package:provider/provider.dart';
 
 import 'booking_details_profile_model.dart';
 export 'booking_details_profile_model.dart';
-
+//for booking confirmation
 class BookingDetailsProfileWidget extends StatefulWidget {
   const BookingDetailsProfileWidget({
     Key? key,
