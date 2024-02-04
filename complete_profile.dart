9f3cb9a174d28ee29eca27f6ac46cde14550a250
Aananditaa <122295513+Tech-neophyte@users.noreply.github.com>
2024-02-05 03:07:17 +0530

    Create complete_profile.dart

--- complete_profile.dart
+++ complete_profile.dart
@@ -13,7 +13,7 @@ import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
-
+//for new users
 class CompleteProfileModel extends FlutterFlowModel {
   ///  State fields for stateful widgets in this page.
 
