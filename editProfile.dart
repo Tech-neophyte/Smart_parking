87c5fb80619b9b40cf87731acccbbcb87c5899a4
Aananditaa <122295513+Tech-neophyte@users.noreply.github.com>
2024-02-05 03:09:47 +0530

    Create editProfile.dart

--- editProfile.dart
+++ editProfile.dart
@@ -16,7 +16,7 @@ import 'package:provider/provider.dart';
 
 import 'edit_profile_model.dart';
 export 'edit_profile_model.dart';
-
+//add changes to database
 class EditProfileWidget extends StatefulWidget {
   const EditProfileWidget({
     Key? key,
