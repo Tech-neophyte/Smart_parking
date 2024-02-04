92cefd7782de45ca23fe0e7ba0082097a04d355c
Aananditaa <122295513+Tech-neophyte@users.noreply.github.com>
2024-02-05 03:06:43 +0530

    Create bookingDetails.dart

--- bookingDetails.dart
+++ bookingDetails.dart
@@ -11,7 +11,7 @@ import 'package:provider/provider.dart';
 
 import 'booking_details_model.dart';
 export 'booking_details_model.dart';
-
+//later added to history
 class BookingDetailsWidget extends StatefulWidget {
   const BookingDetailsWidget({
     Key? key,
